3f35b2628559d9b21612ec1fd31d2889b1cb44de
timmypatrick <70456825+timmypatrick@users.noreply.github.com>
2025-01-28 08:32:52 -0800

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,362 @@
+// Banner Programming
+ScrollReveal().reveal(".banner-image img", {
+  duration: 1000,
+  origin: "right",
+  distance: "200px",
+});
+
+ScrollReveal().reveal(".banner-description", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "200px",
+});
+
+
+// Cloths Programming
+const cloths = {
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+};
+
+ScrollReveal().reveal(".cloth1", {
+  ...cloths,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".cloth2", {
+  ...cloths,
+  delay: 1000,
+});
+
+if (window.innerWidth < 700) {
+  ScrollReveal().reveal(".cloth3", {
+  ...cloths,
+  delay: 500,
+  });
+  
+  ScrollReveal().reveal(".cloth4", {
+  ...cloths,
+  delay: 1000,
+  });
+  
+  ScrollReveal().reveal(".email-sign-up .form", {
+      delay: 500,
+      duration: 1000,
+      origin: "bottom",
+      distance: "200px",
+  });
+
+
+  // About Career
+  ScrollReveal().reveal(".career-image img", {
+    duration: 1000,
+    origin: "right",
+    distance: "100px",
+  });
+
+  ScrollReveal().reveal(".career-text", {
+    delay: 500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+
+  // Mission And Vision Programming
+  ScrollReveal().reveal(".mission", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+  
+  ScrollReveal().reveal(".vision", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+
+  // Contact Programming
+  ScrollReveal().reveal(".developer .phone:first-child .icon", {
+    delay: 4000,
+    duration: 1000,
+    origin: "right",
+    distance: "100px",
+  });
+
+  ScrollReveal().reveal(".developer .phone:last-child .icon", {
+    duration: 1000,
+    origin: "right",
+    distance: "100px",
+  });
+  
+  ScrollReveal().reveal(".developer .phone .write-up", {
+    delay: 500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+} else {
+  ScrollReveal().reveal(".cloth3", {
+  ...cloths,
+  delay: 1500,
+  });
+  
+  ScrollReveal().reveal(".cloth4", {
+  ...cloths,
+  delay: 2000,
+  });
+  
+  ScrollReveal().reveal(".email-sign-up .form", {
+      delay: 500,
+      duration: 1000,
+      origin: "bottom",
+      distance: "200px",
+  });
+
+
+  // About Career
+  ScrollReveal().reveal(".career-infomation", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+  ScrollReveal().reveal(".potential-infomation", {
+    delay: 500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+  ScrollReveal().reveal(".value-infomation", {
+    delay: 1500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+
+  // Mission And Vision Programming
+  ScrollReveal().reveal(".mission", {
+    delay: 2000,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+  
+  ScrollReveal().reveal(".vision", {
+    delay: 2500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+
+
+  // Contact Programming
+  ScrollReveal().reveal(".developer .phone:first-child", {
+    delay: 4000,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+  
+  ScrollReveal().reveal(".developer .phone:last-child", {
+    delay: 4500,
+    duration: 1000,
+    origin: "bottom",
+    distance: "100px",
+  });
+}
+
+
+// Motivation Programming
+ScrollReveal().reveal(".mp1 img", {
+  duration: 1000,
+  origin: "right",
+  distance: "200px",
+});
+
+ScrollReveal().reveal(".mp2", {
+  delay: 500,
+  duration: 1000,
+  origin: "left",
+  distance: "200px",
+});
+
+ScrollReveal().reveal(".motivational-texts", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "200px",
+});
+
+
+// Sign Up Programming
+ScrollReveal().reveal(".sign-up-text", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "200px",
+});
+
+
+// About TP Apparel
+ScrollReveal().reveal(".timmy_patrick img", {
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".about_tp_apparel .description", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+
+// Cart Programming
+ScrollReveal().reveal(".cart-icon", {
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".first-cart-text", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".second-cart-text", {
+  delay: 1000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".cart-link", {
+  delay: 1500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+
+// Thank You Programming
+ScrollReveal().reveal(".greeting-image", {
+  duration: 1000,
+  origin: "top",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".greetings-description", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".thanks", {
+  delay: 1000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+
+
+// FAQs Programming
+ScrollReveal().reveal(".faqs-heading", {
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".quest-ans1", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".quest-ans2", {
+  delay: 1000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".quest-ans3", {
+  delay: 1500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".quest-ans4", {
+  delay: 2000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".quest-ans5", {
+  delay: 2500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+
+// Contact Programming
+ScrollReveal().reveal(".contact1", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact2", {
+  delay: 500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact3", {
+  delay: 1500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact4", {
+  delay: 2000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact5", {
+  delay: 2500,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact6", {
+  delay: 3000,
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".contact7", {
+  delay: 3500,
+  duration: 1000,
+  origin: "right",
+  distance: "100px",
+});
